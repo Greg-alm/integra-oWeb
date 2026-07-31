33741e7de6e2917ec24c8d744e98199557af5cd9
Greg-alm <a.gregory@escola.pr.gov.br>
2026-07-31 11:49:04 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,82 @@
-#Import do framework
-#Import do render_template para leitura HTML
-#request para capturar de dados
-from flask import Flask, render_template, request
-#biblioteca para criar conexão com mysql
-import mysql.connector 
+#import do flask para criação do servidor
+#render_template para criar uma "ponte" com html
+#request para capturar os dados digitados
+from flask import Flask, render_template, redirect, url_for, request
+import mysql.connector
 
+#"Ajuda" o Flask a localizar os caminhos dos arquivos
 app = Flask(__name__)
 
-#Cria conexão com MySQL
 bd_config = {
     'host':'localhost',
     'user':'root',
-    'password':'',
-    'database':'cadastro1'
+    'password':'@',
+    'database':'cadastro'
 }
 
-#Criação de rota para arquivo HTML principal
+#Criando a rota para acessar o arquivo HTML
 @app.route('/')
 def index():
-    return render_template('sla.html')
+    try:
+        #Cria conexão com MySQL e permite adicionar comando SQL
+        conexaoIndex = mysql.connector.connect(**bd_config)
+        cursorIndex = conexaoIndex.cursor(dictionary=True)
 
-@app.route('/cadastrar',methods=['POST'])
-def criar_cadastro():
+        #Seleção da tabela
+        cursorIndex.execute("SELECT CPF, PRIMEIRO_NOME, SOBRENOME, IDADE FROM cliente")
+        lista_clientes = cursorIndex.fetchall()
 
-#recebe os dados do formulario
+        cursorIndex.close()
+        conexaoIndex.close()
+        return render_template('index.html', clientes=lista_clientes)
+    
+    except mysql.connector.Error as err:
+        return f"Erro ao carregar a tabela: {err}"
+    
+#Cria uma rota para acessar o formulário
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    #Bloco para armazenar os dados digiados
     cpf = request.form['cpf']
     primeiro_nome = request.form['primeiro_nome']
-    sobrenome= request.form['sobrenome']
+    sobrenome = request.form['sobrenome']
     idade = request.form['idade']
 
-
     try:
-    #CRIAR CONEXÃO COM O BANCO DE DADOS
-        connect = mysql.connector.connect(**bd_config)
+        #verificando conexão com MySQL
+        conectar = mysql.connector.connect(**bd_config)
+        #Variável que permite a escrever SQL
+        cursor = conectar.cursor()
 
-    #leva instruções SQL do python até o banco de dados
-        caminho = connect.cursor()
+        query = "INSERT INTO cliente(CPF,PRIMEIRO_NOME,SOBRENOME,IDADE) VALUES (%s,%s,%s,%s)"
+        cursor.execute(query,(cpf,primeiro_nome,sobrenome,idade))
 
-        query = "INSERT INTO cliente1 (CPF, PRIMEIRO_NOME,SOBRENOME,IDADE) VALUE (%s,%s,%s,%s)"
-        caminho.execute(query(cpf,primeiro_nome,sobrenome,idade))
+        #Atualiza as alterações e fecha as conexões
+        conectar.commit()
+        cursor.close()
+        conectar.close()
 
-        #salvar as auterações
-        caminho.commit()
-
-        #fechar o cursor
-        caminho.close()
-
-        #fechar a conexão do banco de dados
-        connect.close()
-    except mysql.connector.error as err:
+        return f"<h3>Cliente {primeiro_nome} salvo com sucesso!</h3> <a href='/'>Voltar</a>"
+    
+    except mysql.connector.Error as err:
         return f"Erro ao gravar no banco: {err}"
+
+#Cria a rota para exclusão
+@app.route('/excluir/<cpf>')
+def excluir(cpf):
+    try:
+        conectar = mysql.connector.connect(**bd_config)
+        cursor = conectar.cursor()
+
+        cursor.execute("DELETE FROM cliente WHERE CPF = %s", [cpf])
+
+        conectar.commit()
+        cursor.close()
+        conectar.close()
+
+        return redirect(url_for('index'))
+    except mysql.connector.Error as err:
+        return f"Erro ao excluir: {err}"
+        
+if __name__ == '__main__':
+    app.run(debug=True)
